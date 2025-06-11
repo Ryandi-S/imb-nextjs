d9237706b8ba892006583cdbd0a85e82b4778696
Ryandi-S <93305443+Ryandi-S@users.noreply.github.com>
2025-06-11 14:37:49 +0700

    move to root

--- Test Cloud/src/types/status.ts
+++
@@ -1,9 +0,0 @@
-// Define the status enum
-/**
- * Define the status enum
- * @returns The status enum
- */
-export enum Status {
-  SUCCESS = "success",
-  ERROR = "error",
-}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/app",
+  images: {
+    path: ""
+  }
 };
 
 export default nextConfig;
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
